e05428060648f5673a16189633dae1727c84a207
savuzmir <sebastijan.veselic@gmail.com>
2021-05-26 22:34:16 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,16 @@ import './static/styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render() {
+    return <iframe src="https://opensea.io/assets/crypto-air?embed=true" />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("container"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
